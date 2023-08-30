2fccd7303cb8d788a92faea7e180e4b6c80c55ca
Natanael José <64049748+NatanaelJose@users.noreply.github.com>
2023-08-30 13:38:27 -0300

    lime navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { NavBar } from "./components";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-b from-green-500 to-green-600 w-full overflow-hidden">
+    <div className="bg-gradient-to-b from-lime-700 to-lime-900 w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <NavBar />
       </div>
