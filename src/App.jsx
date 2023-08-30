61658ae12c9301bee56c06fde9302c388527cd6b
Natanael José <64049748+NatanaelJose@users.noreply.github.com>
2023-08-30 13:16:59 -0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { NavBar } from "./components";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-b from-lime-500 to-green-600 w-full overflow-hidden">
+    <div className="bg-gradient-to-r from-green-400 to-blue-500 w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <NavBar />
       </div>
