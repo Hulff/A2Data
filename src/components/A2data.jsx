ddbd73998044b472f2b3bdd9c98a48b2cf7a3edf
Hugo <ph2717693@gmail.com>
2023-11-18 23:03:54 -0300

    css fixes,responsive

--- src/components/A2data.jsx
+++ src/components/A2data.jsx
@@ -12,9 +12,7 @@ const A2Data = () => {
     const [histId, setHistId] = useState('');
     const [data, setData] = useState(null);
     const [histData, setHistData] = useState([]);
-    const endDate = useRef(null)
     const removeListener = useRef(null)
-    const startDate = useRef(null)
     const [years, setY] = useState(null)
     const [monthOpt, setMOp] = useState(null)
     const [yearOpt, setYOp] = useState(null)
@@ -44,7 +42,7 @@ const A2Data = () => {
 
     return (<>
         <div className=' my-6 w-full flex flex-col items-center'>
-            <div className='backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] transition-all flex-col flex items-center h-fit min-h-[20em] w-3/5 flex justify-start text-white bg-gradient-to-r from-purple-800 to-blue-800  rounded-xl pt-4 py-2 pb-6 '>
+            <div className='backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] transition-all flex-col flex items-center h-fit min-h-[20em] md:w-3/12 sm:w-4/12 w-3/4 flex justify-start text-white bg-gradient-to-r from-purple-800 to-blue-800  rounded-xl pt-4 py-2 pb-6 '>
                 {data ? (
                     <>
                         {
@@ -77,8 +75,8 @@ const A2Data = () => {
             </div>
         </div>
 
-        <div className='mb-20 w-full flex items-center flex-col '>
-            <label className=' backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] w-1/2 mb-2 flex items-center text-white bg-gradient-to-r from-purple-800 to-blue-800  rounded-md py-1 pl-3 '>
+        <div className='mb-10 w-full flex items-center flex-col '>
+            <label className='min-h-fit backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)]  mb-2 flex items-center text-white bg-gradient-to-r from-purple-800 to-blue-800  rounded-md py-1 pl-3 '>
                 <AiOutlineSearch className='text-xl' />
                 <input placeholder='Insira o Id do sensor'
                     className='  font-medium bg-transparent focus-visible:outline-0 w-full placeholder:text-white text-white pl-2 py-1  text-md '
@@ -98,7 +96,7 @@ const A2Data = () => {
                     </>
                 ) : (
                     <>
-                        <button className='backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] w-1/3 font-medium text-white rounded-md bg-gradient-to-r from-purple-800 to-blue-800 py-1'
+                        <button className='backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] md:w-2/12 sm:w-1/5 w-1/3 font-medium text-white rounded-md bg-gradient-to-r from-purple-800 to-blue-800 py-1'
                             onClick={() => {
                                 const unsubscribe = onSnapshot(query(collection(db, "sensors", "data", id), orderBy("serverTime", "desc"), limit(1)), (snapshot) => {
                                     let dbData = null;
@@ -124,7 +122,7 @@ const A2Data = () => {
         </div>
 
         <div className=' my-6 w-full flex flex-col items-center'>
-            <div className='hist backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] transition-all flex-col flex items-center max-h-20 min-h-[20em] w-3/5 flex justify-start text-white bg-gradient-to-r from-purple-800 to-blue-800  rounded-xl pt-4 py-2 pb-6 overflow-y-scroll '>
+            <div className='hist backdrop-blur-lg shadow-[0_0_10px_1px_rgba(0,0,0,.25)] transition-all flex-col flex items-center max-h-20 min-h-[20em] md:w-3/12 sm:w-4/12 w-3/4 flex justify-start text-white bg-gradient-to-r from-purple-800 to-blue-800  rounded-xl pt-4 py-2 pb-6 overflow-y-scroll '>
                 {histData.length > 0 ? (
                     <>
                     </>
